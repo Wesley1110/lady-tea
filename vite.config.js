53feac6f29522c9fa25d2acf8b3a92aa12ac5a7a
Wesley1110 <w23453446@gmail.com>
2026-07-10 08:45:17 +0800

    Fix: Switch to CDN tailwind and build successfully

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/lady-tea/', // 🔥 新增這一行，告訴網頁你的 GitHub 專案名稱
-  plugins: [react()],
-})
